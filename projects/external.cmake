22181580783e3cb91650970ae5fd99025ffcf5fa
Kyunghwan Kwon <k@mononn.com>
2024-01-03 11:31:11 +0900

    Add libmcu and unit test framework (#12)

---
+++ projects/external.cmake
@@ -0,0 +1,8 @@
+add_subdirectory(external/libmcu)
+
+target_compile_definitions(libmcu PUBLIC
+	_POSIX_THREADS
+	_POSIX_C_SOURCE=200809L
+	LIBMCU_NOINIT=__attribute__\(\(section\(\".rtc.data.libmcu\"\)\)\)
+	METRICS_USER_DEFINES=\"${PROJECT_SOURCE_DIR}/include/metrics.def\"
+)
