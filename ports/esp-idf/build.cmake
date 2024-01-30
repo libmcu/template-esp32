5947758fbffec5123f743d479bdb6051e1c151ff
Kyunghwan Kwon <k@mononn.com>
2024-01-31 05:45:37 +0900

    fix(build): add empty json strings to avoid project desc. parsing error

--- ports/esp-idf/build.cmake
+++ ports/esp-idf/build.cmake
@@ -31,6 +31,8 @@ set(PROJECT_EXECUTABLE ${CMAKE_PROJECT_NAME}.elf)
 set(PROJECT_BIN ${CMAKE_PROJECT_NAME}.bin)
 set(build_components_json "[]")
 set(build_component_paths_json "[]")
+set(common_component_reqs_json "\"\"")
+set(build_component_info_json "\"\"")
 configure_file("${IDF_PATH}/tools/cmake/project_description.json.in"
 	"${CMAKE_CURRENT_BINARY_DIR}/project_description.json")
 
